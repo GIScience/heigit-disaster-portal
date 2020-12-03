06596ee50bd5bff5ee0b6a0b99641538b5f7a97e
Amandus Butzer <amandus.butzer@heigit.org>
2020-12-03 20:05:41 +0100

    dap_api: update crud_provider.py
    - get_by_owner filter
    - remove redundant code
    
    (cherry picked from commit 600d705713069723e88caa69ff3704472956ad40)

--- dap_api/app/app/crud/crud_provider.py
+++ dap_api/app/app/crud/crud_provider.py
@@ -1,32 +1,29 @@
-from typing import Any, Dict, Optional, Union
+from typing import Optional, List
 
 from sqlalchemy.orm import Session
 
-from . import user
-from .base import CRUDBase
 from app.models.provider import Provider
-from app.schemas.provider import ProviderCreate, ProviderUpdate, ProviderCreateOut
+from app.schemas.provider import ProviderCreate, ProviderUpdate
+from .base import CRUDBase
 
 
 class CRUDProvider(CRUDBase[Provider, ProviderCreate, ProviderUpdate]):
+    def get_multi_by_owner(
+            self, db: Session, *, owner_id: int, skip: int = 0, limit: int = 100
+    ) -> List[Provider]:
+        return (
+            db.query(self.model)
+                .filter(Provider.owner_id == owner_id)
+                .offset(skip)
+                .limit(limit)
+                .all()
+        )
+
     def get_by_email(self, db: Session, *, email: str) -> Optional[Provider]:
         return db.query(Provider).filter(Provider.email == email).first()
 
     def get_by_name(self, db: Session, *, name: str) -> Optional[Provider]:
         return db.query(Provider).filter(Provider.name == name).first()
-
-    def create(self, db: Session, *, obj_in: ProviderCreateOut) -> Provider:
-        db_obj = Provider(
-            email=obj_in.email,
-            name=obj_in.name,
-            description=obj_in.description,
-            owner_id=obj_in.owner_id,
-            owner=user.get(db, obj_in.owner_id)
-        )
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
 
 
 provider = CRUDProvider(Provider)
