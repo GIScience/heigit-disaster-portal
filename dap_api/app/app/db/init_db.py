8135279c8366cb535f83b98ceb8e9d785c66002c
Amandus Butzer <amandus.butzer@heigit.org>
2020-12-04 16:51:39 +0100

    dap_api: Add init user & provider
    Switch to import from collection output
    
    (cherry picked from commit f4ee84e55b944affb983e4a4529f0919772007e6)

--- dap_api/app/app/db/init_db.py
+++ dap_api/app/app/db/init_db.py
@@ -21,38 +21,47 @@ from app.db import import_models  # noqa: F401
 # from app.db.base import BaseTable
 
 
-def add_ds_type_if_missing(db: Session, ds_type: dict, parent_id: int):
-    s_name, s_info = list(ds_type.items())[0]
-    if not crud.disaster_sub_type.get_by_name(db=db, name=s_name):
-        ds_type_obj = schemas.DisasterSubTypeCreate(
-            name=s_name,
-            description=s_info,
-            parent_id=parent_id
-        )
+def create_ds_type_if_missing(db: Session, sub_type: schemas.DisasterSubTypeBaseInDBBase) -> None:
+    if not crud.disaster_sub_type.get_by_name(db=db, name=sub_type.name):
+        ds_type_obj = schemas.DisasterSubTypeCreate(**sub_type.dict())
         crud.disaster_sub_type.create(db=db, obj_in=ds_type_obj)
 
 
-def add_d_type_if_missing(db: Session, d_type):
-    name, info = list(d_type.items())[0]
-    db_entry = crud.disaster_type.get_by_name(db=db, name=name)
-    if not db_entry:
-        d_type_obj = schemas.DisasterTypeCreate(
-            name=name,
-            description=info
-        )
-        db_entry = crud.disaster_type.create(db=db, obj_in=d_type_obj)
+def create_d_type_if_missing(db: Session, d_type: schemas.DisasterTypeBaseInDBBase) -> None:
+    if not crud.disaster_type.get_by_name(db=db, name=d_type.name):
+        d_type_obj = schemas.DisasterTypeCreate(**d_type.dict())
+        crud.disaster_type.create(db=db, obj_in=d_type_obj)
 
-    ds_types = [dict(x) for x in d_type["sub_types"]]
-    for ds_type in ds_types:
-        add_ds_type_if_missing(db, ds_type, db_entry.id)
+    for sub_type in d_type.sub_types:
+        create_ds_type_if_missing(db, sub_type)
 
 
-def init_db(db: Session) -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next line
-    # BaseTable.metadata.create_all(bind=engine)
+def create_provider(db: Session, provider: schemas.ProviderInDB) -> None:
+    if not crud.provider.get_by_name(db=db, name=provider.name):
+        p_obj = schemas.ProviderCreate(**provider.dict())
+        crud.provider.create(db=db, obj_in=p_obj)
 
+
+def create_user(db: Session, user: schemas.User) -> None:
+    if not crud.user.get_by_email(db=db, email=user.email):
+        u_ubj = schemas.UserCreateFromDb(**user.dict())
+        crud.user.create_from_db_entry(db, obj_in=u_ubj)
+
+    for provider in user.providers:
+        create_provider(db, provider)
+
+
+def create_users_and_providers(db: Session) -> None:
+    path = Path(__file__).parent.absolute()
+    with open(f'{path}/init_data/users.json', 'r') as d_type_file:
+        users = json.load(d_type_file)
+
+    users = [schemas.UserInDB(**obj) for obj in users]
+    for user in users:
+        create_user(db, user)
+
+
+def create_admin(db: Session) -> None:
     admin = crud.user.get_by_email(db, email=settings.ADMIN_USER)
     if not admin:
         user_in = schemas.UserCreateOut(
@@ -62,21 +71,33 @@ def init_db(db: Session) -> None:
         )
         user = crud.user.create(db, obj_in=user_in)  # noqa: F841
 
-    # Add disaster types and sub types
+
+def create_disaster_types_and_sub_types(db: Session) -> None:
+    path = Path(__file__).parent.absolute()  # folder path of this file
     # see https://emergency.copernicus.eu/mapping/ems/domains#_ftnref1
-    path = Path(__file__).parent.absolute()
     with open(f'{path}/init_data/d_types.json', 'r') as d_type_file:
         d_types = json.load(d_type_file)
 
-    # convert objects to dict to get .keys(), .values() and .items() functions
-    d_types = [dict(x) for x in d_types]
+    # convert objects to database entry schema
+    d_types = [schemas.DisasterTypeBaseInDBBase(**obj) for obj in d_types]
     for d_type in d_types:
-        add_d_type_if_missing(db, d_type)
+        create_d_type_if_missing(db, d_type)
+
+
+def init_db(db: Session) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # importBaseTable.metadata.create_all(bind=engine)
+
+    create_admin(db)
+    create_users_and_providers(db)
+    create_disaster_types_and_sub_types(db)
 
 
 if __name__ == '__main__':
     import logging
-    from app.db.session import SessionLocal, engine
+    from app.db.session import SessionLocal
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
