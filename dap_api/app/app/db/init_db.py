a25b17976ffe6b3629006e8ec0753fb27989b91f
Amandus Butzer <amandus.butzer@heigit.org>
2020-12-04 12:55:37 +0100

    dap_api: Move init d_type data to file
    Init disaster sub types
    
    (cherry picked from commit 0fc47f0881435c61430d32eb60b4d75573526bd8)

--- dap_api/app/app/db/init_db.py
+++ dap_api/app/app/db/init_db.py
@@ -5,7 +5,9 @@ If we have to initialize the database with users or datasets we can do this here
 # TODO:
 # add Users: Oscar User
 # add Providers: Oscar Provider(from oscar user)
-# add disaster subtypes
+import json
+from pathlib import Path
+
 from sqlalchemy.orm import Session
 
 from app import crud, schemas
@@ -17,6 +19,32 @@ from app.db import import_models  # noqa: F401
 # otherwise, SQL Alchemy might fail to initialize relationships properly
 # for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
 # from app.db.base import BaseTable
+
+
+def add_ds_type_if_missing(db: Session, ds_type: dict, parent_id: int):
+    s_name, s_info = list(ds_type.items())[0]
+    if not crud.disaster_sub_type.get_by_name(db=db, name=s_name):
+        ds_type_obj = schemas.DisasterSubTypeCreate(
+            name=s_name,
+            description=s_info,
+            parent_id=parent_id
+        )
+        crud.disaster_sub_type.create(db=db, obj_in=ds_type_obj)
+
+
+def add_d_type_if_missing(db: Session, d_type):
+    name, info = list(d_type.items())[0]
+    db_entry = crud.disaster_type.get_by_name(db=db, name=name)
+    if not db_entry:
+        d_type_obj = schemas.DisasterTypeCreate(
+            name=name,
+            description=info
+        )
+        db_entry = crud.disaster_type.create(db=db, obj_in=d_type_obj)
+
+    ds_types = [dict(x) for x in d_type["sub_types"]]
+    for ds_type in ds_types:
+        add_ds_type_if_missing(db, ds_type, db_entry.id)
 
 
 def init_db(db: Session) -> None:
@@ -34,141 +62,16 @@ def init_db(db: Session) -> None:
         )
         user = crud.user.create(db, obj_in=user_in)  # noqa: F841
 
+    # Add disaster types and sub types
     # see https://emergency.copernicus.eu/mapping/ems/domains#_ftnref1
-    d_types = [
-        dict({
-            "earthquake": "Earthquake info",
-            "sub_types": [
-                {"ground_shaking": "Ground shaking info"},
-                {"tsunami": "Tsunami info"}
-            ]
-        }),
-        {
-            "volcanic_activity": "Volcanic activity info",
-            "sub_types": [
-                {"ash_fall": "Ash fall info"},
-                {"lahar": "Lahar info"},
-                {"pyroclastic_flow": "Pyroclastic flow info"},
-                {"lava_flow": "Lava flow info"}
-            ]
-        },
-        {
-            "storm": "Storm info",
-            "sub_types": [
-                {"extra_tropical_storm": "Extra-tropical storm info"},
-                {"tropical_cyclone": "Tropical cyclone info"},
-                {"connective_storm": "Connective storm info"}
-            ]
-        },
-        {
-            "extreme_temperature": "Extreme temperature info",
-            "sub_types": [
-                {"cold_wave": "Cold wave info"},
-                {"heat_wave": "Heat wave info"},
-                {"severe_winter_conditions": "Severe winter conditions info"}
-            ]
-        },
-        {
-            "flood": "Flood info",
-            "sub_types": [
-                {"coastal_flood": "coastal_flood info"},
-                {"riverine_flood": "riverine_flood info"},
-                {"flash_flood": "flash_flood info"},
-                {"ice_jam_flood": "ice_jam_flood info"}
-            ]
-        },
-        {
-            "mass_movement": "mass_movement info",
-            "sub_types": [
-                {"landslide": "landslide info"},
-                {"avalanche": "avalanche info"},
-                {"subsidence": "subsidence info"},
-                {"rock_fall": "Rock or debris fall"},
-                {"mudflow": "mudflow info"},
-            ]
-        },
-        {
-            "drought": "drought info",
-            "sub_types": [
-                {"drought": "drought info"}
-            ]
-        },
-        {
-            "wildfire": "wildfire info",
-            "sub_types": [
-                {"forest_fire": "forest_fire info"},
-                {"land_fire": "Bush, brush or Pasture fire"},
-                {"urban_fire": "urban_fire info"}
-            ]
-        },
-        {
-            "epidemic": "epidemic info",
-            "sub_types": [
-                {"viral_disease": "viral_disease info"},
-                {"bacterial_disease": "bacterial_disease info"},
-                {"parasitic_disease": "parasitic_disease info"},
-                {"fungal_disease": "fungal_disease info"},
-                {"prion_disease": "prion_disease info"}
-            ]
-        },
-        {
-            "infestation": "infestation info",
-            "sub_types": [
-                {"grasshopper": "grasshopper info"},
-                {"locust": "locust info"},
-                {"pathogen": "pathogen info"}
-            ]
-        },
-        {
-            "industrial_accident": "industrial_accident info",
-            "sub_types": [
-                {"chemical_spill": "chemical_spill info"},
-                {"collapse": "collapse info"},
-                {"explosion": "explosion info"},
-                {"fire": "fire info"},
-                {"gas_leak": "gas_leak info"},
-                {"poisoning": "poisoning info"},
-                {"radiation": "radiation info"}
-            ]
-        },
-        {
-            "transport_accident": "transport_accident info",
-            "sub_types": [
-                {"air": "air info"},
-                {"road": "road info"},
-                {"rail": "rail info"},
-                {"water": "water info"}
-            ]
-        },
-        {
-            "humanitarian": "humanitarian info",
-            "sub_types": [
-                {"conflict": "conflict info"},
-                {"population_displacement": "Internally displaced persons"},
-                {"rail": "rail info"},
-                {"water": "water info"}
-            ]
-        },
-        {
-            "other": "other info",
-            "sub_types": [
-                {"other": "other info"}
-            ]
-        }
-    ]
+    path = Path(__file__).parent.absolute()
+    with open(f'{path}/init_data/d_types.json', 'r') as d_type_file:
+        d_types = json.load(d_type_file)
+
+    # convert objects to dict to get .keys(), .values() and .items() functions
     d_types = [dict(x) for x in d_types]
     for d_type in d_types:
-        name = list(d_type.keys())[0]
-        db_entry = crud.disaster_type.get_by_name(db=db, name=name)
-        if not db_entry:
-            info = d_type[name]
-            d_type_obj = schemas.DisasterTypeCreate(
-                name=name,
-                description=info
-            )
-            db_entry = crud.disaster_type.create(db=db, obj_in=d_type_obj)
-
-        # TODO: generate Sub types
+        add_d_type_if_missing(db, d_type)
 
 
 if __name__ == '__main__':
@@ -179,6 +82,6 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     logger.info("Creating initial data")
-    db = SessionLocal()
-    init_db(db)
+    db_session = SessionLocal()
+    init_db(db_session)
     logger.info("Initial data created")
