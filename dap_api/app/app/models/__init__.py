07aaaecf06ee96c786cf2a74228fec0bbdf14a91
Amandus Butzer <amandus.butzer@heigit.org>
2020-12-08 20:10:18 +0100

    dap_api: Add d_area db model
    Also including a validity check for polygons
    to ensure no invalid geometry is inserted via
    crud functionality
    
    (cherry picked from commit 39b2f3c9dd659191d4705f59deac10323e950c0f)

--- dap_api/app/app/models/__init__.py
+++ dap_api/app/app/models/__init__.py
@@ -2,3 +2,4 @@ from .provider import Provider
 from .user import User
 from .disaster_type import DisasterType
 from .disaster_sub_type import DisasterSubType
+from .disaster_areas import DisasterArea
---
+++ dap_api/app/app/models/disaster_areas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from geoalchemy2 import Geometry, func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select
+from sqlalchemy.orm import validates
+
+from app.db.base import BaseTable
+
+if TYPE_CHECKING:
+    from .disaster_type import DisasterType  # noqa: F401
+
+
+class DisasterArea(BaseTable):
+    __tablename__ = "disaster_areas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    provider_id = Column(Integer, ForeignKey("providers.id"), nullable=False)
+    d_type_id = Column(Integer, ForeignKey("disaster_types.id"), nullable=False)
+    ds_type_id = Column(Integer, ForeignKey("disaster_sub_types.id"))
+    description = Column(String, index=True)
+    created = Column(DateTime, default=datetime.now())
+
+    geom = Column(Geometry('POLYGON', srid=4326))
+
+    @validates("geom")
+    def validate_geometry(self, key, geom):
+        from app.db.session import SessionLocal
+        db = SessionLocal()
+        valid_check = db.query(func.ST_IsValidReason(geom)).first()[0]
+        if valid_check != "Valid Geometry":
+            raise ValueError(f"Invalid geometry: {valid_check}")
+        return geom
