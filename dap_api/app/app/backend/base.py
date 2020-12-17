ec6c2c312ea2c0dae0280c4deb97bab372f54a89
Amandus Butzer <amandus.butzer@heigit.org>
2020-12-18 00:12:31 +0100

    backend: Make body & header mandatory

--- dap_api/app/app/backend/base.py
+++ dap_api/app/app/backend/base.py
@@ -5,5 +5,5 @@ class BaseProcessor:
     def __init__(self, base_path: str):
         self.base_path = base_path
 
-    def relay_request_post(self, path: str, header: dict = {}, body: dict = {}) -> requests.Response:
+    def relay_request_post(self, path: str, header: dict, body: dict) -> requests.Response:
         return requests.post(url=self.base_path + path, headers=header, json=body)
