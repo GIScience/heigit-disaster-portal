db814a5e0c8caf501bdbeed8ea8f0a97a223b228
Amandus Butzer <amandus.butzer@heigit.org>
2021-01-12 18:30:43 +0100

    api: Move redoc docs to api/v1/redoc

--- dap_api/app/app/main.py
+++ dap_api/app/app/main.py
@@ -7,8 +7,11 @@ from app.config import settings
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/docs/openapi.json",
-    docs_url=f"{settings.API_V1_STR}/docs"
+    docs_url=f"{settings.API_V1_STR}/docs",
+    redoc_url=f"{settings.API_V1_STR}/redoc",
 )
+
+# TODO: different api docs for different api versions.
 
 # Set all CORS enabled origins
 if settings.CORS_ORIGINS:
