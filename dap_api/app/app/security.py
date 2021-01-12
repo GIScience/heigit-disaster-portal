63a611dcbe1332191652d7114043843916d92710
Amandus Butzer <amandus.butzer@heigit.org>
2021-01-12 18:19:28 +0100

    api: Add authorization header dependency
    check_auth_header:
    - adds authorization Header to endpoint
    - raises 401 if passed secret does not belong to a user
    - returns the user model of the secret if found
    
    check_admin_auth:
    - same as above
    - returns 403 if passed secret does not belong to an admin
    
    Endpoints with this dependencies expect a header in form of:
    'Authorization: Bearer <secret>'

--- dap_api/app/app/security.py
+++ dap_api/app/app/security.py
@@ -1,6 +1,8 @@
 import base64
 import secrets
 
+from fastapi.security import HTTPBearer
+
 
 def generate_hash(secret: str) -> str:
     return str(base64.urlsafe_b64encode(bytes(secret, "utf-8")), "utf-8")
@@ -8,3 +10,6 @@ def generate_hash(secret: str) -> str:
 
 def generate_secret(length: int = 32) -> str:
     return secrets.token_urlsafe(length)
+
+
+auth_header = HTTPBearer(auto_error=False)
