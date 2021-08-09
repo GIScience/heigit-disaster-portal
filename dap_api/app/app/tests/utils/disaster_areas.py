3a76cdd5d35f023b840fd56c10dee899571fd628
Amandus Butzer <amandus.butzer@heigit.org>
2021-08-09 16:33:56 +0200

    tests: Add MultiPolygon tests
    add support functions to create MultiPolygons and
    DisasterAreas with multi geometries
    
    relates to https://gitlab.gistools.geog.uni-heidelberg.de/giscience/heigit-disaster-portal/-/issues/6

--- dap_api/app/app/tests/utils/disaster_areas.py
+++ dap_api/app/app/tests/utils/disaster_areas.py
@@ -6,8 +6,21 @@ from sqlalchemy.orm import Session
 from app import crud
 from app.models import DisasterArea
 from app.schemas import DisasterAreaCreate
-from app.schemas.disaster_area import DisasterAreaPropertiesCreate
+from app.schemas.disaster_area import DisasterAreaPropertiesCreate, MultiPolygon
 from app.tests.utils.utils import random_lower_string, random_coordinate
+
+
+def square_around_coordinate_with_padding(
+        c: List[float],
+        p: float
+) -> List[List[List[float]]]:
+    return [[
+        [c[0] - p, c[1] - p],
+        [c[0] + p, c[1] - p],
+        [c[0] + p, c[1] + p],
+        [c[0] - p, c[1] + p],
+        [c[0] - p, c[1] - p]]
+    ]
 
 
 def create_new_polygon(
@@ -18,13 +31,20 @@ def create_new_polygon(
         c = random_coordinate(-10, 10)
     x = 1 * f
     return Polygon(
-        coordinates=[[
-            [c[0] - x, c[1] - x],
-            [c[0] + x, c[1] - x],
-            [c[0] + x, c[1] + x],
-            [c[0] - x, c[1] + x],
-            [c[0] - x, c[1] - x]
-        ]]
+        coordinates=square_around_coordinate_with_padding(c, x)
+    )
+
+
+def create_new_multi_polygon(
+        c: List[float] = None,
+        f: float = 0.0001
+) -> MultiPolygon:
+    if c is None:
+        c = random_coordinate(-10, 10)
+    x = 1 * f
+    return MultiPolygon(
+        coordinates=[square_around_coordinate_with_padding(c, x),
+                     square_around_coordinate_with_padding([n + 3 * x for n in c], x)]
     )
 
 
@@ -53,7 +73,8 @@ def create_new_disaster_area(
         name: str = None,
         p_id: int = 1,
         d_id: int = 1,
-        info: str = None
+        info: str = None,
+        multi: bool = False
 
 ) -> DisasterArea:
     if c is None:
@@ -63,7 +84,7 @@ def create_new_disaster_area(
     if info is None:
         info = random_lower_string(16)
     d_area = DisasterAreaCreate(
-        geometry=create_new_polygon(c, f),
+        geometry=create_new_multi_polygon(c, f) if multi else create_new_polygon(c, f),
         properties=DisasterAreaPropertiesCreate(
             name=name,
             d_type_id=d_id,
