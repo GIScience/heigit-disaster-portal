6a5514cb32bdc8c87a764bdb3684c0560b177156
Amandus Butzer <amandus.butzer@heigit.org>
2022-04-21 15:40:49 +0200

    ui: Self-host JS and CSS for OpenAPI UIs
    the interactive documentation (OpenAPI and Redoc) were served
    from a CDN and are now served from local files to improve privacy.
    
    Also added HeiGIT favicon.

--- dap_api/app/app/main.py
+++ dap_api/app/app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
@@ -7,9 +13,39 @@ from app.config import settings
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/docs/openapi.json",
-    docs_url=f"{settings.API_V1_STR}/docs",
-    redoc_url=f"{settings.API_V1_STR}/redoc",
+    docs_url=None,
+    redoc_url=None
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get(f"{settings.API_V1_STR}/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/favicon.ico",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get(f"{settings.API_V1_STR}/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+        redoc_favicon_url="/static/favicon.ico",
+        with_google_fonts=False,
+    )
 
 # TODO: different api docs for different api versions.
 
