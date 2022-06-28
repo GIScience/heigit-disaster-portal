47c7e6476cc3d34852f84f3c3a5753d48a4e39bd
Amandus Butzer <amandus.butzer@heigit.org>
2022-06-28 13:50:58 +0200

    test: fix email string type

--- dap_api/app/app/tests/utils/utils.py
+++ dap_api/app/app/tests/utils/utils.py
@@ -2,6 +2,8 @@ import random
 import string
 from typing import Dict, List
 
+from pydantic import EmailStr
+
 from app.config import settings
 
 
@@ -9,8 +11,8 @@ def random_lower_string(length: int = 32) -> str:
     return "".join(random.choices(string.ascii_lowercase, k=length))
 
 
-def random_email() -> str:
-    return f"{random_lower_string(12)}@{random_lower_string(4)}.com"
+def random_email() -> EmailStr:
+    return EmailStr(f"{random_lower_string(12)}@{random_lower_string(4)}.com")
 
 
 def random_coordinate(min_value: int, max_value: int) -> List[float]:
