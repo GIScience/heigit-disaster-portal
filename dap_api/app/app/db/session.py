674bc2a5a51b6094d812140028a37147f0790808
Amandus Butzer <amandus.butzer@heigit.org>
2022-07-01 15:28:49 +0200

    api.db: increase postgis connection capacity
    by default the sqlalchemy db connection allows for 5
    simultaneous connections and 10 overflow connections.
    This would result in deadlocks on the db for 50 requests
    per second.
    
    Postgres internally allows up to 100 connections by default.
    With the new pool size of 20 and an overflow of 50
    the portal should be able to handle a decent request amount.
    
    Still requests should normally be sent in sequence, and not simultaneously.
    Especially for long running requests with complex geometries.

--- dap_api/app/app/db/session.py
+++ dap_api/app/app/db/session.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_timeout=60,
+    pool_size=20,
+    max_overflow=50  #
     # connect_args={"check_same_thread": False}  # only needed for SQLite DB
 )
 
