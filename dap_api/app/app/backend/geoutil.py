e6254237dc7d6b0dea0f2dd3ff74c593b4b2e0db
Amandus Butzer <amandus.butzer@heigit.org>
2022-07-20 15:51:07 +0200

    backend.geoutil: Add more type hints and documentation

--- dap_api/app/app/backend/geoutil.py
+++ dap_api/app/app/backend/geoutil.py
@@ -4,8 +4,10 @@ from typing import List, Union
 import geopy
 import geopy.distance
 from geoalchemy2 import func
+from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import Function
 
+from app.schemas import OrsApi, OrsResponseType
 
 
 def point_from_point_bearing_distance(lon: float, lat: float, bearing: float, distance: float) -> (float, float):
@@ -15,7 +17,7 @@ def point_from_point_bearing_distance(lon: float, lat: float, bearing: float, di
     @param lat: latitude
     @param bearing: in degree
     @param distance: in meters
-    @return:
+    @return: (lon, lat) point
     """
     lon_prc = lat_prc = 7
     if bearing in [0, 180]:
@@ -28,19 +30,19 @@ def point_from_point_bearing_distance(lon: float, lat: float, bearing: float, di
     return round(goal.longitude, lon_prc), round(goal.latitude, lat_prc)
 
 
-def point_distance(lon1, lat1, lon2, lat2):
+def point_distance(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     """
     return distance in meters between two geographic points
-    @param lon1:
-    @param lat1:
-    @param lon2:
-    @param lat2:
-    @return:
+    @param lon1: point 1 longitude
+    @param lat1: point 1 latitude
+    @param lon2: point 2 longitude
+    @param lat2: point 2 latitude
+    @return: distance between point 1 and 2 in meters
     """
     return geopy.distance.geodesic((lat1, lon1), (lat2, lon2)).meters
 
 
-def bbox_length(bbox):
+def bbox_length(bbox: List[float]) -> float:
     """
      return max of bbox width / length, not used now but potentially in future versions to improve
      calculations in bbox_buffer_percentage
@@ -52,7 +54,7 @@ def restrict_longitude(lon: float) -> Union[float, int]:
     """
     Restricts longitude to valid range of -180 to 180
     @param lon: longitude
-    @return: valid lon
+    @return: valid longitude
     """
     if -180 <= lon <= 180:
         return int(lon) if float(lon).is_integer() else lon
@@ -66,7 +68,7 @@ def restrict_latitude(lat: float) -> Union[float, int]:
     """
     Restricts latitude to valid range of -90 to 90
     @param lat: latitude
-    @return: valid lat
+    @return: valid latitude
     """
     if lat > 90:
         return 90
@@ -82,7 +84,7 @@ def buffer_bbox(bbox: List[Union[float, int]], p: float = 0, d: float = 0) -> Li
     @param bbox: bbox to buffer
     @param p: buffer percentage
     @param d: buffer distance
-    @return:
+    @return: buffered bbox
     """
     lon_dist = (bbox[2] - bbox[0]) * p / 100
     lat_dist = (bbox[3] - bbox[1]) * p / 100
@@ -100,7 +102,7 @@ def bbox_from_radius(lon: float, lat: float, radius: float) -> List[Union[float,
     @param lon: center longitude
     @param lat: center latitude
     @param radius: of the circle in meters for which the bbox should be calculated
-    @return:
+    @return: calculated bbox
     """
     lon1, _ = point_from_point_bearing_distance(lon, lat, 270, radius)
     _, lat1 = point_from_point_bearing_distance(lon, lat, 180, radius)
@@ -114,17 +116,17 @@ def bbox_from_radius(lon: float, lat: float, radius: float) -> List[Union[float,
     ]
 
 
-def meters_travelled(seconds, speed):
+def meters_travelled(seconds: float, speed: float) -> int:
     """
     return meters travelled in x seconds for a specific speed in kmh
-    @param seconds:
-    @param speed:
-    @return:
+    @param seconds: travel time in seconds
+    @param speed: travel speed in km/h
+    @return: travel distance in meters
     """
     return round(seconds * speed / 3600 * 1000)
 
 
-def build_diff_query(avoid_item, item, ors_api, ors_res_type) -> Function:
+def build_diff_query(avoid_item: dict, item: dict, ors_api: OrsApi, ors_res_type: OrsResponseType) -> Function:
     """
     build the sql query to calculate the geometric difference between
     corresponding isochrone or route items.
@@ -150,25 +152,25 @@ def build_diff_query(avoid_item, item, ors_api, ors_res_type) -> Function:
     return query
 
 
-def get_bbox_for_encoded_polyline(db, item):
+def get_bbox_for_encoded_polyline(db: Session, item: dict) -> Union[float, int]:
     """
     Returns the bbox for the encoded polyline of a json route item
     @param db: db session
     @param item: route item in json format
     @return: bbox for the encoded polyline route
     """
-    geom = get_geom_from_item(item, "json")
+    geom = get_geom_from_item(item, OrsResponseType.json)
     bbox_geojson = db.execute(func.ST_AsGeoJson(func.Box2D(geom), 5, 1)).scalar()
     return json.loads(bbox_geojson)["bbox"]
 
 
-def get_geom_from_item(item: dict, ors_res_type):
+def get_geom_from_item(item: dict, ors_res_type: OrsResponseType) -> Function:
     """
     Extract the geometry from the correct place in the response item
     depending on the response format
     @param item: single route or isochrone item
     @param ors_res_type: response format
-    @return: item of the ors response
+    @return: geoalchemy function for PostGIS geometry of the ors response
     """
     geom = item.get('geometry')
     if ors_res_type == 'geojson':
@@ -180,9 +182,9 @@ def get_geom_from_item(item: dict, ors_res_type):
 
 def get_overall_bbox(bboxes: List[List[float]]) -> List[float]:
     """
-    Returns bbox for multiple bboxes
+    Returns bbox for multiple bboxes (2D or 3D)
     @param bboxes: list of bboxes
-    @return:
+    @return: bbox containing every bbox of the passed list
     """
     tuples = [x for x in list(zip(*bboxes))]
     bbox = []
