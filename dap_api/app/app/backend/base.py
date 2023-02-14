7d5e5aa6d030cb33ebd12802826c7a6e44fb9ae1
christina <christina.ludwig@uni-heidelberg.de>
2023-02-14 15:25:05 +0100

    feat(dap_api): extend BaseProcessor
    make base_path adjustable in relay_request_post function
    
    Co-authored-by: Rafael Troilo <rafael.troilo@heigit.org>

--- dap_api/app/app/backend/base.py
+++ dap_api/app/app/backend/base.py
@@ -7,9 +7,11 @@ class BaseProcessor:
     def __init__(self, base_path: str):
         self.base_path = base_path
 
-    def relay_request_post(self, path: str, header: dict, body: dict) -> requests.Response:
+    def relay_request_post(self, path: str, header: dict, body: dict, base_path: str = None) -> requests.Response:
+        if not base_path:
+            base_path = self.base_path
         try:
-            return requests.post(url=self.base_path + path, headers=header, json=body)
+            return requests.post(url=base_path + path, headers=header, json=body)
         except requests.exceptions.ConnectionError as e:
             raise HTTPException(
                 status_code=500,
